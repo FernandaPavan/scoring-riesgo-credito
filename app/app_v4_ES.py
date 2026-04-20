a06f57dcf8de46699868408e104f05ea681f62a3
Fernanda Pavan <fernandacpavan@gmail.com>
2026-04-20 02:48:51 -0300

    ajuste 6

--- app/app_v4_ES.py
+++ app/app_v4_ES.py
@@ -25,49 +25,30 @@ bins_woe = joblib.load(os.path.join(MODEL_PATH, "woe_bins.pkl"))
 # ============================================
 # ============================================
 # ============================================
-# CONFIG
-# ============================================
-st.set_page_config(layout="wide")
-
 # ============================================
 # CSS
 # ============================================
 st.markdown("""
 <style>
 
-/* TÍTULOS DAS SEÇÕES */
-.seccion {
-    text-align:center;
-    color:#2563eb;
-    font-size:22px;
-    font-weight:600;
-}
-
-/* SCORE */
-.score {
-    text-align:center;
-    font-size:60px;
-    font-weight:700;
-}
-
-/* ABAS - CINZA MÉDIO */
+/* ABAS - CINZA CLARO */
 button[data-baseweb="tab"] {
-    font-size: 26px !important;
+    font-size: 22px !important;
     font-weight: 700 !important;
-    color: #6b7280 !important;
+    color: #9ca3af !important;
 }
 
 /* TEXTO INTERNO */
 button[data-baseweb="tab"] p {
-    font-size: 26px !important;
+    font-size: 22px !important;
     font-weight: 700 !important;
-    color: #6b7280 !important;
+    color: #9ca3af !important;
 }
 
 /* ABA SELECIONADA */
 button[data-baseweb="tab"][aria-selected="true"] {
-    color: #6b7280 !important;
-    border-bottom: 3px solid #6b7280 !important;
+    color: #9ca3af !important;
+    border-bottom: 3px solid #9ca3af !important;
 }
 
 </style>
